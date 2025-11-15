cd66454aa349196a994b5d97170520dcc4cdb65e
aerobis <185216831+aerobis@users.noreply.github.com>
2025-11-15 18:00:58 +0545

    Solved exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,11 +1,17 @@
-const removeFromArray = function(array, ...args) {
+const removeFromArray = function(array, ...args){
+    let currentArg = [];
     for (let i = 0; i < args.length; i++){
-        if(array.includes(args[i])){
-            array.splice(array.indexOf(args[i]), 1);
+        currentArg[0] = args[i];
+        for(let j = array.length - 1; j >= 0; j--){
+            if(currentArg[0] == array[j]){
+                array.splice(j, 1);
+            }
         }
     }
-    return array;
-};
+    console.log(array)
+}
+
+removeFromArray([1,2,3,4,5], 1, 2, 3, 5)
 
 // Do not edit below this line
 module.exports = removeFromArray;
