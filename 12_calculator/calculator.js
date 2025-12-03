c219c5adc814f6c101b5310356cc1c89fa8f8dbd
aerobis <185216831+aerobis@users.noreply.github.com>
2025-12-03 19:53:29 +0545

    Completed exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,50 +1,50 @@
-const add = function(num1, num2) {
-	return num1 + num2;
-};
+function add(num1, num2){
+  return num1+num2;
+}
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
-};
+function subtract(num1, num2){
+  return num1-num2;
+}
 
-const sum = function(array) {
+function sum(arr){
   let sum = 0;
-	for(let i = 0; i < array.length; i++){
-    sum += (array[i]);
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
   }
   return sum;
 };
 
-const multiply = function(array) {
-  let multiply = 0;
-  for (let i = 0; i < array.length; i++){
-    multiply *= (array[i]);
+function multiply(arr){
+  let product = 1;
+  for(let i = 0; i<arr.length; i++){
+    product *= arr[i];
   }
-  return multiply;
-};
+  return product;
+}
 
-const power = function(base, exponent) {
-  return Math.pow(base, exponent);
-};
+function power(base, exponent){
+  let result = 1;
+  for(let i = 1; i <= exponent; i++){
+    result *= base;
+  }
+  return result;
+}
 
-const factorial = function(num) {
-  let result;
-
-	if (num == 0){
+function factorial(num){
+  if(num == 0){
     return 1;
   }
+
   if (num == 1){
     return 1;
   }
-  if (num == 2){
+
+  if(num == 2){
     return 2;
   }
 
-  while (num != 0){
-    result = num * num - 1;
-  }
-
-  return result;
-};
+  return num * factorial(num-1);
+}
 
 // Do not edit below this line
 module.exports = {
