027a51a9dd6310ee99dc312d4de50a078b01940d
eckspur <185216831+aerobis@users.noreply.github.com>
2026-08-30 23:35:22 +0930

    Completed recursion/2

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,14 @@
-const contains = function() {
-  
+const contains = function (value, searchValue) {
+  if (value === searchValue) return true;
+
+  if (Number.isNaN(value) && Number.isNaN(searchValue)) return true;
+
+  if (value === null || typeof value !== "object") return false;
+
+  const nestedValues = Object.values(value);
+
+  return nestedValues.some((nestedValue) => contains(nestedValue, searchValue));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
