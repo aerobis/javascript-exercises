c2e6ed4b968adc1b0d3a862cf5babd325857efdb
eckspur <185216831+aerobis@users.noreply.github.com>
2026-08-30 19:13:28 +0930

    Completed recursion/1

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function(num) {
+    if(typeof num !=  "number" || Number.isNaN(num) || num % 1 != 0 || num < 0){
+        return undefined;
+    }
 
+    if(num == 0 || num == 1){
+        return 1;
+    }
+
+    return num * factorial( num - 1 );
 };
 
 // Do not edit below this line
