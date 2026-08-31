26e18ba23c074e66825433b8eb31bfadc13fad53
eckspur <185216831+aerobis@users.noreply.github.com>
2026-08-31 10:02:43 +0930

    Completed recursion/3

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,22 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(input) {
+    let sum = 0;
+    if(typeof input == "number"){
+        return Number.isInteger(input) ? 1 : 0;
+    }
+
+    if(typeof input !== "object" || input === null){
+        return undefined;
+    }
+
+    const nestedValues = Object.values(input);
+    nestedValues.forEach((nestedValue)=> {
+        const result = totalIntegers(nestedValue);
+        if(result !== undefined){
+            sum += result;
+        }
+    });
+
+    return sum;
 };
   
 // Do not edit below this line
